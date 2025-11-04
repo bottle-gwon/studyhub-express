91a1c0e806bda99a760aaa2000a72b999d23eaf0
nari-0v0 <ej9811@naver.com>
2025-11-04 11:09:38 +0900

    feat : 익스프레스 더미데이터 세팅 #9

---
+++ src/interfaces/_recruitManageInterfaces.ts
@@ -0,0 +1,29 @@
+export interface Manage {
+  id: number
+  uuid: string
+  title: string
+  content: string
+  thumbnail_img_url: string
+  expected_headcount: number
+  current_headcount: number
+  estimated_fee: number
+  views_count: number
+  bookmark_count: number
+  due_date: string
+  is_closed: boolean
+  tags: { id: number; name: string }[]
+  lectures: { id: number; title: string; instructor: string }[]
+  study_group: {
+    id: number
+    uuid: string
+    name: string
+  }
+  author: {
+    id: number
+    nickname: string
+    profile_img_url: string
+  }
+  is_bookmarked: boolean
+  created_at: string
+  updated_at: string
+}
