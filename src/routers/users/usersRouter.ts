a818ba3c5f19e7c4291537820420c58cefacfd1f
bottle-gwon <lazyguy1@daum.net>
2025-11-05 15:27:52 +0900

    chore: 더미데이터 작성중 #14

---
+++ src/routers/users/usersRouter.ts
@@ -0,0 +1,8 @@
+import express from 'express'
+
+const usersRouter = express.Router()
+
+usersRouter.get('/chat/rooms', async () => {
+  // const page = Number(req.query.page ?? 1)
+  // const page_size = Number(req.query.page_size ?? 5)
+})
