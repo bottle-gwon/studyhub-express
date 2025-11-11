432796e6f37ee3cadf686cf010a9fd6de3131541
bottle-gwon <lazyguy1@daum.net>
2025-11-11 16:45:52 +0900

    fix: 채팅 메시지 조회 api 명세에 따라 최신화 #60

--- src/interfaces/_chatInterfaces.ts
+++ src/interfaces/_chatInterfaces.ts
@@ -6,12 +6,16 @@ export interface ChatRoomListItem {
   last_message_content: string
   created_at: string
 }
+interface MessageSender {
+  nickname: string
+  id: number
+}
 export interface Message {
   id: number
   is_read: boolean
   content: string
-  sender_nickname: string
-  sender_id: number
+  sender: MessageSender
+  // sender_nickname: string
+  // sender_id: number
   created_at: string
-  updated_at: string
 }
